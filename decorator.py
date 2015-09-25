662fab0288bae3ab7782fbfe3ecad13d85be4335
hanjian <e4e555@163.com>
2015-09-25 11:00:40 +0800

    add distributed

--- decorator.py
+++ decorator.py
@@ -1,12 +1,21 @@
-def deco(func):
-    def wrapper():
-        print("begin")
-        func()
-        print("end")
-    return wrapper
+import functools
+def log(text = None):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(args, **kw):
+                if text == None:
+                    print('call %s():' %func.name)
+                else:
+                    print('%s %s():' %(text, func.name))
+                return func(args, **kw)
+            return wrapper
+        return decorator
 
-@deco
-def myfunc():
-    print("myfunc called")
-
-myfunc()
+@log()
+def now1():
+    print('2015-9-15')
+@log('test')
+def now2():
+    print('2015-9-15')
+print(now1())
+print(now2())
